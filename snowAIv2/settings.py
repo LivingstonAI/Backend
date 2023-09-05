8efd3b48aab0d53686053c5d6e1ff94f2f6c0564
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-09-05 23:11:14 +0200

    Update settings.py

--- snowAIv2/settings.py
+++ snowAIv2/settings.py
@@ -139,3 +139,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'https://frontend-2yg4.vercel.app/',
+    'http://localhost:5173/'
+]
