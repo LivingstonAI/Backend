1f02046abcd173bacea58f1c79a724e9886cb30e
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-09-06 10:00:19 +0200

    Update settings.py

--- snowAIv2/settings.py
+++ snowAIv2/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Backend.middleware.open_access_middleware'
+    'django.middleware.open_access_middleware'
 ]
 
 ROOT_URLCONF = 'snowAIv2.urls'
