ecc42c5861a3e5dab15d4092e974f7e290a43341
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-09-06 08:37:45 +0200

    Update settings.py

--- snowAIv2/settings.py
+++ snowAIv2/settings.py
@@ -37,10 +37,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://frontend-2yg4.vercel.app",
     "http://localhost:5173"
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    "https://frontend-2yg4.vercel.app",
-    "http://localhost:5173"
-]
+
+CORS_ORIGIN_WHITELIST = ('http://localhost:5173',)
 
 AUTH_USER_MODEL = 'snowAIWeb.User'
 
