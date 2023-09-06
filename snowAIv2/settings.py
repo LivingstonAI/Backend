12b3fa2f4945ee274a0b4ae1466f3e07717fe659
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-09-06 10:23:41 +0200

    Update settings.py

--- snowAIv2/settings.py
+++ snowAIv2/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     'snowAIWeb',
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
