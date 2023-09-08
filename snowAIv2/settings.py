536247f26b92dad8395dac38182188bb49a28b96
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-09-08 08:18:11 +0200

    new commit

--- snowAIv2/settings.py
+++ snowAIv2/settings.py
@@ -124,12 +124,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'GBeLlohB2ve4O8tZ0vYS',
-        'HOST': 'containers-us-west-186.railway.app',
-        'PORT': '7171',
-        # 'PASSWORD': os.environ['PASSWORD'],
-        # 'HOST': os.environ['HOST'],
-        # 'PORT': os.environ['PORT'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': os.environ['PORT'],
     }
 }
 
