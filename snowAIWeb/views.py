f72ea402002dfae3fc79d11c9b37813cc4b8670e
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-19 08:35:56 +0200

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -30,7 +30,7 @@ import urllib.parse
 # from backtesting.test import SMA, GOOG, EURUSD
 import pandas as pd
 # import pandas_ta as ta
-
+import os
 
 current_hour = datetime.datetime.now().time().hour
 
@@ -681,7 +681,7 @@ def fetch_news_data(request):
 @csrf_exempt
 def moving_average_bot(request, type_1, type_2, ma1, ma2):
 
-    df = pd.read_csv('/snowAIWeb/XAUUSD.csv').drop_duplicates()
+    df = os.path.join(os.path.dirname(os.path.realpath(__file__)), './XAUUSD.csv')
     df.index = pd.to_datetime(df['Time'].values)
     del df['Time']
     # bt = Backtest(df, SmaCross,exclusive_orders=False, cash=10000)
