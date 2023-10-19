dfa5854cd431bfe47991d0236052fadd6407ce22
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-19 15:26:13 +0200

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -691,10 +691,10 @@ async def handle_api_request(type_1, type_2, ma1, ma2):
 @csrf_exempt
 async def moving_average_bot(request, type_1, type_2, ma1, ma2):
     # Use asyncio.gather to run the asynchronous function concurrently
-    results = await asyncio.gather(await handle_api_request(type_1, type_2, ma1, ma2))
+    results = asyncio.gather(await handle_api_request(type_1, type_2, ma1, ma2))
 
     # Process the results and return the response
-    return JsonResponse({'Output': await results})
+    return JsonResponse({'Output': results})
 
 
 # https://backend-production-c0ab.up.railway.app/create-bot/sma/ema/200/50
