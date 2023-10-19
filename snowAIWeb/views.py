ad1da91056ba458ac1f97b5a33786e486be5144c
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-19 12:16:13 +0200

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -683,7 +683,7 @@ async def handle_api_request(type_1, type_2, ma1, ma2):
     df = pd.read_csv(df_path).drop_duplicates()
     df.index = pd.to_datetime(df['Time'].values)
     del df['Time']
-    bt = Backtest(df, SmaCross, exclusive_orders=False, cash=10000)
+    bt = await Backtest(df, SmaCross, exclusive_orders=False, cash=10000)
     output = await bt.run()
     return output
 
