2de61d2854379ac9876c680f827e20d303594c22
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-19 15:41:00 +0200

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -678,18 +678,18 @@ def fetch_news_data(request):
 
 
 @csrf_exempt
-async def handle_api_request(type_1, type_2, ma1, ma2):
-    df_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), './XAUUSD.csv')
-    df = pd.read_csv(df_path).drop_duplicates()
-    df.index = pd.to_datetime(df['Time'].values)
-    del df['Time']
-    # bt = Backtest(df, SmaCross, exclusive_orders=False, cash=10000)
-    output = 'hi there!'
-    return output
-
-
-@csrf_exempt
 async def moving_average_bot(request, type_1, type_2, ma1, ma2):
+
+
+    async def handle_api_request(type_1, type_2, ma1, ma2):
+        df_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), './XAUUSD.csv')
+        df = pd.read_csv(df_path).drop_duplicates()
+        # df.index = pd.to_datetime(df['Time'].values)
+        # del df['Time']
+        # bt = Backtest(df, SmaCross, exclusive_orders=False, cash=10000)
+        output = 'hi there!'
+        return output
+
     # Use asyncio.gather to run the asynchronous function concurrently
     results = await handle_api_request(type_1, type_2, ma1, ma2)
 
