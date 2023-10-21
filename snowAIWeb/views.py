0f797f7fea7ddcc7d8101d7dbac64f9753ed1272
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-21 11:16:44 +0200

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -655,13 +655,13 @@ def fetch_news_data(request):
     # Check if news data for the current day already exists
     existing_news = News.objects.filter(day_created=today)
     
-    if existing_news.exists():
-        # If data for the current day exists, return a message indicating it
-        return JsonResponse({'message': 'News data for today already exists.'})
-    else:
-        # If data for the current day doesn't exist, fetch and save news data
-        # save_news_data()
-        return JsonResponse({'message': 'News data for today does not exist'})
+    # if existing_news.exists():
+    #     # If data for the current day exists, return a message indicating it
+    #     return JsonResponse({'message': 'News data for today already exists.'})
+    # else:
+    #     # If data for the current day doesn't exist, fetch and save news data
+    #     # save_news_data()
+    #     return JsonResponse({'message': f'News data for today does not exist {str(today)}'})
     
     # Fetch all news data without using serializers
     news_objects = News.objects.all()
@@ -672,7 +672,8 @@ def fetch_news_data(request):
         news_data.append({
             "symbol": news.symbol,
             "description": news.data,
-            "created_on": news.day_created
+            "created_on": news.day_created,
+            'today': today
         })
     
     # # Convert the list to JSON and return it
