079cd592b985cd8f669c2ecda4438350dec61b5b
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-23 09:42:20 +0200

    new commit

--- snowAIWeb/urls.py
+++ snowAIWeb/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     # path('save_news_data', views.save_news_data, name='save_news_data'),
     path('create-bot/<str:type_1>/<str:type_2>/<int:ma1>/<int:ma2>', views.moving_average_bot, name='create-bot'),
     path('create-bot/bbands/<int:length>/<int:std>', views.bbands_bot, name='create-bot-bbands'),
-    path('create-bot/rsi/<int:length>/<int:overbought_level>/<int:oversold_level>', views.rsi_bot, name='create-bot-rsi'),
+    path('create-bot/rsi/<int:length>/<int:overboughtLevel>/<int:oversoldLevel>', views.rsi_bot, name='create-bot-rsi'),
     # path('fetch_user_email', views.fetch_user_email, name='fetch_user_email')
 ]
 
