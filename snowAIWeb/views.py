2d71d2ce58f18566abd347a1096f61d930cfc7aa
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-24 22:35:27 +0200

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -1752,7 +1752,7 @@ async def handle_api_request_candlesticks(engulfing, pinbar, morning_star, three
     del df['Time']
     df["SMA_200"] = ta.sma(df["Close"], length=200)
     df["EMA_50"] = ta.ema(df["Close"], length=50)
-    test_length = int(len(df) * 0.35)
+    test_length = int(len(df) * 0.5)
     bt = Backtest(df[:test_length], Strat, exclusive_orders=False, cash=10000)
     output = bt.run()
     
