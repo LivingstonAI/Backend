7b2ae6a650b7f6d70a6042a646fe140ab0191770
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-10-31 11:24:59 +0200

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -843,7 +843,7 @@ async def handle_api_request(type_1, type_2, ma1, ma2):
     del df['Time']
     length = int(len(df) * 0.35)
     second_length = int(len(df)* 0.7)
-    bt = Backtest(df[length:second_length], SmaCross, exclusive_orders=False, cash=10000)
+    bt = Backtest(df[:length], SmaCross, exclusive_orders=False, cash=10000)
     output = bt.run()
     
     # Convert the relevant output fields to a dictionary
