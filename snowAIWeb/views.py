cc8209f898353b95495b4ca1dcb3e367555c3b5d
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2023-12-21 11:52:38 +0000

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -3725,7 +3725,7 @@ def update_news_data(request):
         print("Received currencies:", currencies)
 
         # Send back a JSON response indicating success
-        return JsonResponse({'message': 'success'})
+        return JsonResponse({'message': f'{currencies}'})
 
     # Handle other HTTP methods or invalid requests
     return JsonResponse({'message': 'Invalid request'}, status=400)
