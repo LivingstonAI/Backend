57e623e9338ee51e9929fae8679e5187d2010611
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-03-26 11:40:30 +0000

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -3815,11 +3815,11 @@ def genesys(request):
         try:
             data = json.loads(request.body)
             generated_code = data.get('generatedCode', '')
-            # generated_code = generated_code.remove('\n')
+            generated_code = generated_code.replace('\n', '')
             # Process the generated code as needed
             # Here, you can include your logic to compile or execute the generated Python code
             # For demonstration purposes, I'll simply return it in the JSON response
-            return JsonResponse({generated_code})
+            return JsonResponse({f'generatedCode': generated_code})
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
     else:
