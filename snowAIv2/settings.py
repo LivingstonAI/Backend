e711685488c98aba624b52b9196b8f93bb01cd44
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-04-07 16:20:02 +0000

    new commit

--- snowAIv2/settings.py
+++ snowAIv2/settings.py
@@ -80,13 +80,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'snowAIv2.middleware.CustomCorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
