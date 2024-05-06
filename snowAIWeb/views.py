5cadff97f0c9799f2ef84082fa8942036c4b4ef1
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-05-06 11:26:06 +0000

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -4455,6 +4455,8 @@ def set_init_capital(request):
 def genesys_live(request, identifier, initial_equity, trade_equity, current_equity, num_positions):
     
     return_statement = None
+
+    print(f'Identifier: {identifier}\nInitial Equity: {initial_equity}\nTrade Equity: {trade_equity}\nNum Positions: {num_positions}\n')
     
     def set_take_profit(number, type_of_setting):
         # current_equity = equity
@@ -4464,6 +4466,7 @@ def genesys_live(request, identifier, initial_equity, trade_equity, current_equi
                 
         if type_of_setting == 'PERCENTAGE':
             percentage = ((current_equity - trade_equity) / initial_equity) * 100
+            print(f'Percentage is: {percentage}')
             if percentage >= number:
                 return_statement = "close_position"
         elif type_of_setting == 'NUMBER':
