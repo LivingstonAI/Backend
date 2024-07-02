0b142588e6cb203157d7dfceae85a149ef0c7ff4
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-07-02 05:14:57 +0000

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -4910,8 +4910,8 @@ def delete_unique_bot(request, bot_id):
 @csrf_exempt
 def clear_stuff(request):
     uniqueBot.objects.all().delete()
-    # tradeModel.objects.all().delete()
-    # Trade.objects.all().delete()
+    tradeModel.objects.all().delete()
+    Trade.objects.all().delete()
 
     return JsonResponse({"message": f"All models deleted!"})
 
