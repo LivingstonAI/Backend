af0bcc4e906566f6682030c689ba0b13dcc2db3c
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-10-22 08:12:49 +0000

    new commit

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -5701,7 +5701,7 @@ def create_chill_data(request):
         ## Who is the market maker?
         ### The Central Banks are the producer of Currency Prices and are the "Market Maker." Currency prices are 100% controlled and manipulated by the Central Banks. Investment Bank Dealers and or Brokers are not Market Makers. Dealers and Brokers do business with the Price Feeds set and delivered by Central Banks.
 
-        ## What should the maret be experiencing at the present?
+        ## What should the market be experiencing at the present?
         ### Bullish or Bearish
 
         ## Where do you find the information?
@@ -5712,17 +5712,23 @@ def create_chill_data(request):
         ### COT shows a visual representation of how smart money positions itself.
 
         ### Traders don't move markets, Central Banks do (currency prices).
-
         '''
+        
+        # Check if a Chill instance with the same section already exists
+        if Chill.objects.filter(section=section).exists():
+            return JsonResponse({'message': 'Chill Data with this section already exists!'}, status=400)
+        
+        # If not, save the new data
         chill_data = Chill(
             section=section,
             text=text
         )
         chill_data.save()
-        return JsonResponse({'message': f'Chill Data Saved Successfully!'})
+        return JsonResponse({'message': 'Chill Data Saved Successfully!'}, status=201)
+    
     except Exception as e:
         print(f'error in Chill Data Function: {e}')
-        return JsonResponse({'message': f'error in Chill Data Function: {e}'})
+        return JsonResponse({'message': f'error in Chill Data Function: {e}'}, status=500)
 
 
 
