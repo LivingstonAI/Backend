2f1c121ecf31940e6bb927fa9e60f9b147b89872
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2024-12-18 10:51:53 +0000

    The Kingdom of God is within you.

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -6108,24 +6108,20 @@ def get_accounts(request):
     return JsonResponse(accounts_data, safe=False)
 
 
-# Create a new account
 @csrf_exempt
 def create_account(request):
     if request.method == "POST":
         data = json.loads(request.body)
         account_name = data.get("name")
         initial_capital = data.get("initial_capital")
-        main_assets = data.get("main_assets")  # Get main_assets from the request body
+        main_assets = data.get("main_assets")  # Get main_assets from request
 
         if account_name and initial_capital is not None and main_assets:
-            account = Account.objects.create(
-                account_name=account_name,
-                initial_capital=initial_capital,
-                main_assets=main_assets  # Store main_assets as a string (comma-separated)
-            )
+            account = Account.objects.create(account_name=account_name, initial_capital=initial_capital, main_assets=main_assets)
             return JsonResponse({"message": "Account created successfully!", "id": account.id}, status=201)
-        return JsonResponse({"error": "Invalid data"}, status=400)
         
+        return JsonResponse({"error": "Invalid data"}, status=400)
+
 
 # Delete an account
 @csrf_exempt
