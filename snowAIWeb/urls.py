ca2738d430ce16ed3901de8589d781d6f2a78fc7
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2025-12-29 22:19:01 +0200

    Update urls.py

--- snowAIWeb/urls.py
+++ snowAIWeb/urls.py
@@ -457,6 +457,8 @@ urlpatterns = [
      path('api/mss-hyper-volumetric-relativistic-analyzer/', views.mss_hyper_volumetric_relativistic_analyzer, name='mss-hyper-volumetric-relativistic-analyzer'),
      path('api/mss-quantum-sector-momentum-flux-analyzer/', views.mss_quantum_sector_momentum_flux_analyzer, name='mss-quantum-sector-momentum-flux-analyzer'),
      path('api/mss-stock-sector-relativistic-performance-comparator/', views.mss_stock_sector_relativistic_performance_comparator, name='mss-stock-sector-relativistic-performance-comparator'),
+     path('api/mss-stock-sector-identifier/', views.mss_stock_sector_identifier, name='mss-stock-sector-identifier'),
+
 
 
 
