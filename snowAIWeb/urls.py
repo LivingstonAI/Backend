5947f792a4b05704caf2ed79b4d816d33ccb1d26
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2026-02-27 12:47:51 +0200

    Update urls.py

--- snowAIWeb/urls.py
+++ snowAIWeb/urls.py
@@ -721,7 +721,10 @@ urlpatterns = [
       name='snowai_thundervault_ohlcv_chart_stream',
   ),
 
- path('ideas_hub_trend_age_analysis_v1', views.ideas_hub_trend_age_analysis_v1, name='ideas_hub_trend_age_analysis_v1'),
+  path('ideas_hub_trend_age_analysis_v1', views.ideas_hub_trend_age_analysis_v1, name='ideas_hub_trend_age_analysis_v1'),
+
+   path('ideas_hub_stock_info_v1', views.ideas_hub_stock_info_v1, name='ideas_hub_stock_info_v1'),
+
 
  path('ideas_hub_chart_data_v1', views.ideas_hub_chart_data_v1, name='ideas_hub_chart_data_v1'),
 
