e4b708167a9e92a3da001caa1b69279980ebf8dd
Tlotlo <114500548+butterrobot2022@users.noreply.github.com>
2026-04-20 16:52:04 +0000

    new comet

--- snowAIWeb/views.py
+++ snowAIWeb/views.py
@@ -29224,13 +29224,13 @@ def prepare_namespace(model, dataset):
     return namespace
     
 
-scheduler.add_job(
-    execute_all_forward_tests,
-    trigger=IntervalTrigger(minutes=5),
-    id='snowai_forward_test_job',
-    name='Execute all SnowAI forward tests every 5 minutes',
-    replace_existing=True
-)
+# scheduler.add_job(
+#     execute_all_forward_tests,
+#     trigger=IntervalTrigger(minutes=5),
+#     id='snowai_forward_test_job',
+#     name='Execute all SnowAI forward tests every 5 minutes',
+#     replace_existing=True
+# )
 
 
 # ============================================================================
